bc46c84bb686b07fbe3c1084a34acb80ff61fe8c
Sam Stephens <stephens@reservoir.com>
2020-05-11 21:51:54 -0400

    readded clean, added env formatting, many smaller fixes. think it's working with matmult now?

--- src/arcc_main.py
+++ src/arcc_main.py
@@ -7,6 +7,7 @@ import os
 import pathlib
 import random
 import re
+import shutil
 import subprocess as sp
 import time
 from abc import ABC, abstractmethod
@@ -16,7 +17,6 @@ from typing import List, Optional, Any, Dict, Iterator, Tuple, Union
 
 import src.meta_data_info
 import src.meta_data_parser
-
 
 
 def get_logger() -> logging.Logger:
@@ -60,12 +60,13 @@ class Config:
     Configuration class produced during "production", and consumed during
     "consumption".
     """
-    def __init__(self, build: str, run: str,
+    def __init__(self, clean: str, build: str, run: str,
                  root: "TunableArg", max_iter: Optional[int],
-                 output: pathlib.Path, search_strategy: "SearchStrategy"):
-        # build command
+                 output: pathlib.Path, search_strategy: "SearchStrategy",
+                 preserve_files: List[pathlib.Path], fresh_dir: bool):
+        # clean/build/run commands
+        self.clean = clean
         self.build = build
-        # run command
         self.run = run
         # root of all arguments - uses a tree structure
         self.root = root
@@ -75,6 +76,10 @@ class Config:
         self.output = output
         # search strategy (random, mutation, etc) class
         self.search_strategy = search_strategy
+        # files to preserve
+        self.preserve_files = preserve_files
+        # whether to switch to a fresh directory
+        self.fresh_dir = fresh_dir
 
     def run_iter(self) -> Iterator[int]:
         """
@@ -212,15 +217,15 @@ class DiscreteRange(DataRange):
 class StringFormatter:
     """
     Wrapper around a string that's meant to be formatted in the context of an
-    argument. For example, "$VAL < $CHILDA + $CHILDB" when expanded with a node
-    would expand to the string where $VAL is substitued for the value of the
-    current node and $CHILDA and $CHILDB are substituted for the values of the
-    children with the same name, or error if they don't exist.
+    argument. For example, "{VAL} < {CHILDA} + {CHILDB}" when expanded with an
+    arg would expand to the string where $VAL is substituted for the value of
+    the current node and $CHILDA and $CHILDB are substituted for the values of
+    the children with the same name, or error if they don't exist.
     """
     def __init__(self, format_str: str):
         self.format_str = format_str
 
-    def expand(self, assignment: Union[Any, "Assignment"]) -> str:
+    def expand_assignment(self, assignment: Union[Any, "Assignment"]) -> str:
         def find_replacement(match) -> str:
             """
             regex sub requires a function that takes in a match and returns
@@ -241,20 +246,31 @@ class StringFormatter:
             return str(val)
         return re.sub(r"{(\w+)}", find_replacement, self.format_str)
 
+    def expand_dict(self, with_dict: Dict[str, str]) -> str:
+        """
+        expand the key, but using a known dictionary to find substitutions. in
+        theory, expand_assignment could be rewritten by building the full dict
+        and then calling this, but that could be quite slow.
+        """
+        return re.sub(r"{(\w+)}", lambda match: with_dict[match.group(1)],
+                      self.format_str)
+
 
 class TunableArg:
     """
     Class representing an argument or group of arguments that can be
     automatically tuned.
     """
-    def __init__(self, name: str, set_env: Optional[StringFormatter],
+    def __init__(self, name: str, env_key: Optional[StringFormatter],
+                 set_env: Optional[StringFormatter],
                  constraint: Optional[StringFormatter],
                  children: List["TunableArg"],
                  data_range: Optional[DataRange]):
         # name given to this arg. used for setting command-line/environment.
         # NOTE: the "full name" will be ...GRANDPARENT_NAME.PARENT_NAME.MY_NAME
         self.name = name
-        # how the environment variable should be formatted
+        # how the environment variable key and value should be formatted
+        self.env_key = env_key
         self.set_env = set_env
         # how the constraint should be formatted
         self.constraint = constraint
@@ -305,7 +321,14 @@ class TunableArg:
             # reverse direction. I'm not sure how this is parsed on the
             # R-Stream side of things.
             if self.set_env:
-                tr['_'.join(new_path)] = self.set_env.expand(assignment)
+                # expand the user-given key to get our key
+                # NOTE: this could be done in __init__ time
+                if self.env_key:
+                    env_key = self.env_key.expand_dict(
+                        {"name": self.name, "path": '_'.join(new_path)})
+                else:
+                    env_key = '_'.join(new_path)
+                tr[env_key] = self.set_env.expand_assignment(assignment)
         # build the remainder path recursively
         for child_name, child in self.children.items():
             tr.update(child.build_env(
@@ -318,7 +341,7 @@ class TunableArg:
         """
         # if we have a constraint, expand, evaluate, and interpret as a bool
         if self.constraint is not None:
-            expanded = self.constraint.expand(assignment)
+            expanded = self.constraint.expand_assignment(assignment)
             res = eval(expanded)
             assert isinstance(res, bool)
             if not res:
@@ -343,11 +366,18 @@ def convert_classic_meta_data(
     args = []
     # returned in a singly linked list structure
     while meta_data is not None:
+        prefix = meta_data.ID + "_"
+        # the old parser appends the parent variable's name to the child
+        # variables for some reason. strip that off to clean up a bit
+
+        def strip_prefix(old: str) -> str:
+            assert old.startswith(prefix)
+            return old[len(prefix):]
         children = []
         # add all the children variables
         for var, options in zip(meta_data.var_list, meta_data.var_vals_list):
             children.append({
-                "name": var,
+                "name": strip_prefix(var),
                 "range": {
                     "discrete": options
                 }
@@ -358,14 +388,20 @@ def convert_classic_meta_data(
         def convert_old_to_new(old: str) -> str:
             # match any word-like string inside of dollar signs.
             # this will greedy (avoid matching `$VAR1$ $VAR2$`), as desired.
-            return re.sub(r"\$(\w+)\$", lambda m: f"{{{m.group(1)}}}", old)
-        group_data = {
+            return re.sub(r"\$(\w+)\$",
+                          lambda m: f"{{{strip_prefix(m.group(1))}}}", old)
+        arg = {
             "name": meta_data.ID,
+            "env_key": "ARCC_OPTION_{name}",
             "set_env": convert_old_to_new(meta_data.option),
-            "constraint": convert_old_to_new(meta_data.constraint),
             "children": children,
         }
-        args.append(group_data)
+        # avoid appending the constraint if it's statically true
+        constraint = convert_old_to_new(meta_data.constraint)
+        if constraint != "True":
+            arg["constraint"] = constraint
+
+        args.append(arg)
         meta_data = meta_data.next
     return {"args": args}
 
@@ -386,7 +422,9 @@ def parse_args() -> Any:
                         help="configuration file with tunable args, and "
                              "optionally build/run commands. See README "
                              "for more info.")
-    # build and run command
+    # clean build and run command
+    parser.add_argument('--clean',
+                        help="clean command. ran before building.")
     parser.add_argument('--build',
                         help="build command. use {VAR_NAME} to indicate where "
                              "variables should be placed in the command.")
@@ -403,10 +441,18 @@ def parse_args() -> Any:
     parser.add_argument('--max-iter', '-n', type=int,
                         help="max number of iterations in the search "
                              "(infinite by default)")
-    # output file
+    # output folder
+    curr_time = str(datetime.now()).replace(' ', '_')
+    default_output = os.path.join("arcc-codes", f"arcc-run-{curr_time}")
     parser.add_argument('--output', help="output location",
-                        default=str(pathlib.Path.cwd().joinpath(
-                            "arcc-codes", f"arcc-run-{datetime.now()}")))
+                        default=default_output)
+    # files to copy to our own directory
+    parser.add_argument('--preserve', help="files to preserve in our local dir",
+                        nargs="+")
+    # whether to switch to a fresh directory to run our command
+    parser.add_argument('--fresh-dir',
+                        help="switch to a fresh directory to run our command",
+                        action='store_true')
 
     # exclusive group of search strategies
     strategy_group = parser.add_mutually_exclusive_group()
@@ -437,6 +483,9 @@ def production(args: Any) -> Config:
         meta_data = src.meta_data_parser.parse(config_file)
         # conversion step
         data = convert_classic_meta_data(meta_data)
+        # TODO: remove this
+        with open('arcc-new.json', 'w') as f:
+            json.dump(data, f, indent=2)
     elif args.config:
         # new format
         config_file = pathlib.Path(args.config)
@@ -452,7 +501,7 @@ def production(args: Any) -> Config:
     # NOTE: the "clean" stage is no longer necessary, as each test is ran in a
     # separate folder.
     stages = []
-    for stage in ["build", "run"]:
+    for stage in ["clean", "build", "run"]:
         file_data = data.get(stage)
         cmd_data = getattr(args, stage)
         # by default, use the cmd line argument
@@ -471,7 +520,7 @@ def production(args: Any) -> Config:
                           f"config file or command line args"
     # parse the tunable args from the config file, and add a GLOBAL root arg
     # that contains all of them
-    root = TunableArg("GLOBAL", None, None,
+    root = TunableArg("GLOBAL", None, None, None,
                       [parse_arg(arg, None) for arg in data["args"]], None)
 
     # choose and initialize the search strategy class with the tunable arguments
@@ -484,9 +533,13 @@ def production(args: Any) -> Config:
     else:
         get_logger().info("defaulting to random search strategy")
         search_strategy = RandomSearch
+    if args.preserve is None:
+        args.preserve = []
+    args.preserve = [pathlib.Path(file) for file in args.preserve]
     search_strategy = search_strategy(root)
-    return Config(stages[0], stages[1], root,
-                  args.max_iter, pathlib.Path(args.output), search_strategy)
+    return Config(stages[0], stages[1], stages[2], root,
+                  args.max_iter, pathlib.Path(args.output), search_strategy,
+                  args.preserve, args.fresh_dir)
 
 
 def parse_arg(datum: Any, path: Optional[List[str]]) -> TunableArg:
@@ -517,7 +570,8 @@ def parse_arg(datum: Any, path: Optional[List[str]]) -> TunableArg:
 
     # check if there are any unexpected keys, and warn if so. Do this first, as
     # it may be an indicator of an error to come.
-    possible_keys = ["name", "set_env", "constraint", "range", "children"]
+    possible_keys = ["name", "env_key", "set_env",
+                     "constraint", "range", "children"]
     for key in datum:
         if key not in possible_keys:
             get_logger().warning(f"unknown key {key} in {get_path()}")
@@ -526,6 +580,12 @@ def parse_arg(datum: Any, path: Optional[List[str]]) -> TunableArg:
     set_env = datum.get("set_env")
     if set_env is not None:
         set_env = StringFormatter(set_env)
+    env_key = datum.get("env_key")
+    if env_key is not None:
+        assert set_env is not None, \
+            f"env_key specified but not set_env in {get_path()}: " \
+            f"can't set environment variable key without value"
+        env_key = StringFormatter(env_key)
 
     # constraint formatter
     constraint = datum.get("constraint")
@@ -563,7 +623,7 @@ def parse_arg(datum: Any, path: Optional[List[str]]) -> TunableArg:
     # recursively parse all our children
     children = [parse_arg(child, path) for child in datum.get("children", [])]
 
-    return TunableArg(name, set_env, constraint, children, range_data)
+    return TunableArg(name, env_key, set_env, constraint, children, range_data)
 
 
 def consumption(config: Config):
@@ -576,40 +636,48 @@ def consumption(config: Config):
     config.output.mkdir(parents=True)
 
     for run_id in config.run_iter():
-        # get the next assignment to try from the strategy
-        assignment = config.search_strategy.get_assignment()
-        # the strategy finished early, exit
+        while True:
+            # get the next assignment to try from the strategy
+            assignment = config.search_strategy.get_assignment()
+            # the strategy finished early, exit
+            if assignment is None:
+                break
+            # valid assignment! continue on
+            if config.root.is_valid_assignment(assignment):
+                break
+        # must have finished early
         if assignment is None:
             break
-        # invalid assignment, continue
-        if not config.root.is_valid_assignment(assignment):
-            continue
-        # create the new directory to run the test in
-        out_dir = config.output.joinpath(f"run-{run_id}")
-        out_dir.mkdir()
+
+        # create the new directory
+        fresh_dir = config.output.joinpath(f"run-{run_id}")
+        fresh_dir.mkdir()
 
         # write extra info to log.txt as we run
-        with open(str(out_dir.joinpath("log.txt")), "x") as out_file:
+        with open(str(fresh_dir.joinpath("log.txt")), "x") as out_file:
             # store the runtime and whether an error occurred
             runtime = None
             error_occurred = False
-            get_logger().info(f"run {run_id} with assignment "
-                              f"{assignment}")
-            print(str(assignment), file=out_file)
-            for stage in ["build", "run"]:
+            get_logger().info(f"run {run_id} with assignment:\n{assignment}")
+            print(f"assignment: {assignment}", file=out_file)
+            for stage in ["clean", "build", "run"]:
                 cmd = getattr(config, stage)
-                run_env = os.environ.copy()
                 if stage == "build":
                     # update the command and environment using the assignment
-                    cmd = StringFormatter(cmd).expand(assignment)
+                    cmd = StringFormatter(cmd).expand_assignment(assignment)
                     new_env = config.root.build_env(None, assignment)
-                    run_env = run_env.update(new_env)
+                else:
+                    new_env = {}
+
+                run_env = os.environ.update(new_env)
                 # run the stage command, and handle any errors appropriately
-                get_logger().debug(f"running {stage} stage with cmd `{cmd}` "
-                                   f"and env `{new_env}`")
-                print(f"{out_dir}: {cmd}, env={new_env}", file=out_file)
+                get_logger().debug(f"running stage `{stage}` with cmd `{cmd}`")
+                if len(new_env) > 0:
+                    get_logger().debug(f"and env {new_env}")
+                run_dir = fresh_dir if config.fresh_dir else pathlib.Path.cwd()
+                print(f"{fresh_dir}: {cmd}, env={new_env}", file=out_file)
                 start = time.time()
-                proc = sp.run(cmd, shell=True, cwd=str(out_dir),
+                proc = sp.run(cmd, shell=True, cwd=str(run_dir),
                               stdout=sp.PIPE, stderr=sp.STDOUT,
                               encoding='utf-8', env=run_env)
                 end = time.time()
@@ -622,6 +690,7 @@ def consumption(config: Config):
                     error_str = f"failed to run {stage} with code " \
                                 f"{proc.returncode}"
                     print(error_str, file=out_file)
+                    get_logger().debug(proc.stdout)
                     get_logger().error(error_str)
                     error_occurred = True
                     break
@@ -629,16 +698,23 @@ def consumption(config: Config):
             if not error_occurred:
                 get_logger().info(f"successfully ran in {runtime}s")
                 config.search_strategy.notify_results(assignment, runtime)
+            # preserve any files the user has requested
+            for file in config.preserve_files:
+                if not file.exists():
+                    get_logger().warning(f"can't find file {file} to preserve")
+                else:
+                    shutil.copy(str(file), str(fresh_dir.joinpath(file.name)))
 
     # finished all runs! clean up and print results
-    get_logger().info(f"finished executing")
+    get_logger().info(f"finished executing in {config.output}")
     optimal = config.search_strategy.get_optimal()
     if optimal is None:
         get_logger().info("no optimal assignment found")
     else:
         assignment, runtime = optimal
-        get_logger().info(f"optimal assignment is {assignment} with runtime "
-                          f"{runtime}s")
+        get_logger().info(f"optimal assignment is:\n"
+                          f"{assignment}\n"
+                          f"runtime: {runtime}s")
 
 
 class Assignment:
@@ -653,14 +729,25 @@ class Assignment:
     @classmethod
     def from_path_assignments(cls, paths: List[Tuple[List[str], Any]]) \
             -> "Assignment":
+        """
+        Build an assignment from a raw path assignment.
+        """
+        # this is the raw map that will be used at the end
         items = {}
+        # because we need all paths with the same prefix to go together,
+        # store them here
         raw = defaultdict(lambda: list())
         for path, val in paths:
             assert len(path) > 0
+            # if the path has length one, there must not be any others with the
+            # same path
             if len(path) == 1:
+                assert path[0] not in items
+                assert path[0] not in raw
                 items[path[0]] = val
             else:
                 raw[path[0]].append((path[1:], val))
+        # for each path with the same prefix, build recursively the remainder
         for path0, remaining in raw.items():
             items[path0] = Assignment.from_path_assignments(remaining)
         return Assignment(items)
@@ -701,8 +788,12 @@ class Assignment:
                 tr += [([child], val)]
         return tr
 
+    def flattened_str(self) -> str:
+        return '\n'.join(f"{'.'.join(key)}: {val}"
+                         for key, val in self.path_assignments())
+
     def __str__(self) -> str:
-        return json.dumps(self.as_json())
+        return self.flattened_str()
 
     # forward hash/eq to the immutable representation
     def __hash__(self):
@@ -718,7 +809,7 @@ class SearchStrategy(ABC):
     """
 
     @abstractmethod
-    def __init__(self, root: TunableArg):
+    def __init__(self, _root: TunableArg):
         """
         Must be able to initialize with just the tunable arguments.
         """
@@ -900,4 +991,3 @@ def main():
     args = parse_args()
     config = production(args)
     consumption(config)
-
